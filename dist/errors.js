4ca4b6c81b02a20ccc608037b0fc49c6823691c7
Contra <contra@users.noreply.github.com>
2017-05-05 17:58:19 -0400

    build

--- dist/errors.js
+++ dist/errors.js
@@ -1,23 +1,23 @@
-"use strict";
+'use strict';
 
 Object.defineProperty(exports, "__esModule", {
   value: true
 });
 exports.NotFoundError = exports.BadRequestError = exports.codes = undefined;
 
-var _getPrototypeOf = require("babel-runtime/core-js/object/get-prototype-of");
+var _getPrototypeOf = require('babel-runtime/core-js/object/get-prototype-of');
 
 var _getPrototypeOf2 = _interopRequireDefault(_getPrototypeOf);
 
-var _classCallCheck2 = require("babel-runtime/helpers/classCallCheck");
+var _classCallCheck2 = require('babel-runtime/helpers/classCallCheck');
 
 var _classCallCheck3 = _interopRequireDefault(_classCallCheck2);
 
-var _possibleConstructorReturn2 = require("babel-runtime/helpers/possibleConstructorReturn");
+var _possibleConstructorReturn2 = require('babel-runtime/helpers/possibleConstructorReturn');
 
 var _possibleConstructorReturn3 = _interopRequireDefault(_possibleConstructorReturn2);
 
-var _inherits2 = require("babel-runtime/helpers/inherits");
+var _inherits2 = require('babel-runtime/helpers/inherits');
 
 var _inherits3 = _interopRequireDefault(_inherits2);
 
@@ -34,13 +34,15 @@ var codes = exports.codes = {
 var BadRequestError = exports.BadRequestError = function (_Error) {
   (0, _inherits3.default)(BadRequestError, _Error);
 
-  function BadRequestError(message, status) {
+  function BadRequestError() {
+    var message = arguments.length > 0 && arguments[0] !== undefined ? arguments[0] : 'Bad Request';
+    var status = arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : codes.badRequest;
     (0, _classCallCheck3.default)(this, BadRequestError);
 
     var _this = (0, _possibleConstructorReturn3.default)(this, (BadRequestError.__proto__ || (0, _getPrototypeOf2.default)(BadRequestError)).call(this, message));
 
     _this.message = message;
-    _this.status = status || codes.badRequest;
+    _this.status = status;
     return _this;
   }
 
@@ -50,13 +52,15 @@ var BadRequestError = exports.BadRequestError = function (_Error) {
 var NotFoundError = exports.NotFoundError = function (_Error2) {
   (0, _inherits3.default)(NotFoundError, _Error2);
 
-  function NotFoundError(message, status) {
+  function NotFoundError() {
+    var message = arguments.length > 0 && arguments[0] !== undefined ? arguments[0] : 'Not Found';
+    var status = arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : codes.notFound;
     (0, _classCallCheck3.default)(this, NotFoundError);
 
     var _this2 = (0, _possibleConstructorReturn3.default)(this, (NotFoundError.__proto__ || (0, _getPrototypeOf2.default)(NotFoundError)).call(this, message));
 
     _this2.message = message;
-    _this2.status = status || codes.notFound;
+    _this2.status = status;
     return _this2;
   }
 
