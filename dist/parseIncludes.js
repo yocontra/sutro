c5384c45385ec53026a48a7726ddbefc70d4a1fa
contra <yo@contra.io>
2019-01-10 18:25:25 -0500

    4.2.1

--- dist/parseIncludes.js
+++ dist/parseIncludes.js
@@ -6,19 +6,24 @@ const toArray = i => {
   if (Array.isArray(i)) return i;
   return [i];
 };
+const toStringArray = i => toArray(i).map(s => String(s));
 
 const parseIncludes = include => {
   const inp = toArray(include);
   return inp.map((i, idx) => {
     if (typeof i === 'string') i = { resource: i };
     if (!i || typeof i !== 'object') throw new Error(`Invalid include: ${idx}`);
+    if (typeof i.resource !== 'string') throw new Error(`Invalid include resource: ${idx}`);
 
-    return {
-      resource: i.resource,
-      includes: i.includes && parseIncludes(i.includes),
-      attributes: i.attributes && toArray(i.attributes),
-      limit: parseInt(i.limit)
-    };
+    // tack on to keep it minimal
+    const out = { resource: i.resource };
+    if (i.includes) out.includes = parseIncludes(i.includes);
+    if (i.attributes) out.attributes = toStringArray(i.attributes);
+    if (i.limit) {
+      const limit = parseInt(i.limit);
+      if (!isNaN(limit)) out.limit = limit;
+    }
+    return out;
   });
 };
 
